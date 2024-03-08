f4e15948e56964eae561ab0180486d84a6603c19
mitaka621 <d418201@gmail.com>
2024-03-09 01:38:22 +0200

    added custom middleware which check if the user is deleted and logges them out if needed

---
+++ HomeAssistant/Middlewares/IsDeletedCheckMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using HomeAssistant.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
+namespace HomeAssistant.Middlewares
+{
+	public class IsDeletedCheckMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public IsDeletedCheckMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context, UserManager<HomeAssistantUser> userManager)
+		{
+			// Check the "isDeleted" property and log out the user if true
+			var user = await userManager.GetUserAsync(context.User);
+			if (user != null && user.IsDeleted)
+			{
+				// Log out the user
+					
+				await context.SignOutAsync(IdentityConstants.ApplicationScheme);
+			}
+
+			// Continue with the request pipeline
+			await _next(context);
+		}
+	}
+}
---
+++ HomeAssistant/Middlewares/IsDeletedCheckMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeAssistant.Middlewares
+{
+	public static class IsDeletedCheckMiddlewareExtension
+	{
+
+		public static IApplicationBuilder UseIsDeletedCheck(
+			this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<IsDeletedCheckMiddleware>();
+		}
+	}
+}
