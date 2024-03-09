1d848212294d45e2b41ec6823e26b78b420ed123
mitaka621 <d418201@gmail.com>
2024-03-10 00:31:31 +0200

    user can now chnage first, last name and add phonenumber in usersettings

--- HomeAssistant/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ HomeAssistant/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -9,156 +9,186 @@ using System.Threading.Tasks;
 using HomeAssistant.Core.Contracts;
 using HomeAssistant.Core.Services;
 using HomeAssistant.Infrastructure.Data.Models;
+using HomeAssistant.Infrastructure.Migrations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeAssistant.Areas.Identity.Pages.Account.Manage
 {
-    public class IndexModel : PageModel
-    {
-        private readonly UserManager<HomeAssistantUser> _userManager;
-        private readonly SignInManager<HomeAssistantUser> _signInManager;
-        private readonly IPFPService _pfpService;
+	public class IndexModel : PageModel
+	{
+		private readonly UserManager<HomeAssistantUser> _userManager;
+		private readonly SignInManager<HomeAssistantUser> _signInManager;
+		private readonly IPFPService _pfpService;
 
-        public IndexModel(
-            UserManager<HomeAssistantUser> userManager,
-            SignInManager<HomeAssistantUser> signInManager,
-            IPFPService ipFPService)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _pfpService = ipFPService;
-        }
+		public IndexModel(
+			UserManager<HomeAssistantUser> userManager,
+			SignInManager<HomeAssistantUser> signInManager,
+			IPFPService ipFPService)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_pfpService = ipFPService;
+		}
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string Username { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public string Username { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		[TempData]
+		public string StatusMessage { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		[BindProperty]
+		public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
-        {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public class InputModel
+		{
+			/// <summary>
+			///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+			///     directly from your code. This API may change or be removed in future releases.
+			/// </summary>
+			[Phone]
+			[Display(Name = "Phone number")]
+			public string PhoneNumber { get; set; }
 
-            public IFormFile ImageFile { get; set; }
-        }
+			[Required]
+			[Display(Name = "First Name")]			
+			public string FirstName { get; set; }
 
-        private async Task LoadAsync(HomeAssistantUser user)
-        {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+			[Required]
+			[Display(Name = "Last Name")]
+			public string LastName { get; set; }
 
-            Username = userName;
+			public IFormFile ImageFile { get; set; }
+		}
 
-            Input = new InputModel
-            {
-                PhoneNumber = phoneNumber
-            };
+		private async Task LoadAsync(HomeAssistantUser user)
+		{
+			var userName = await _userManager.GetUserNameAsync(user);
+			var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
-            var imageData = await _pfpService.GetImage(user.Id);
+			Username = userName;
 
-            if (imageData != null && imageData.Length > 0)
-            {
 
-                ViewData["ImageData"] = imageData;
-            }
-            else
-            {
-                ViewData["ImageData"] = null;
-            }
-        }
+			Input = new InputModel
+			{
+				PhoneNumber = phoneNumber,
+				FirstName = user.FirstName,
+				LastName = user.LastName
+			};
 
-        public async Task<IActionResult> OnGetAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+			var imageData = await _pfpService.GetImage(user.Id);
 
-            await LoadAsync(user);
+			if (imageData != null && imageData.Length > 0)
+			{
 
-           
-            return Page();
-        }
+				ViewData["ImageData"] = imageData;
+			}
+			else
+			{
+				ViewData["ImageData"] = null;
+			}
+		}
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            Task uploadPfpTask = null;
-            if (Input.ImageFile != null && Input.ImageFile.Length > 0)
-            {
-                using (var stream = new MemoryStream())
-                {
-                    Input.ImageFile.CopyTo(stream);
-                    var imageData = stream.ToArray();
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+			}
 
-                    var userId = _userManager.GetUserId(User);
+			await LoadAsync(user);
 
-                    uploadPfpTask = _pfpService.SaveImage(userId, imageData);
-                }
 
-            }
+			return Page();
+		}
 
-            
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			Task uploadPfpTask = null;
+			if (Input.ImageFile != null && Input.ImageFile.Length > 0)
+			{
+				using (var stream = new MemoryStream())
+				{
+					Input.ImageFile.CopyTo(stream);
+					var imageData = stream.ToArray();
 
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
+					var userId = _userManager.GetUserId(User);
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult =await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+					uploadPfpTask = _pfpService.SaveImage(userId, imageData);
+				}
 
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
+			}
 
-            if (uploadPfpTask!=null)
-            {
-                await uploadPfpTask;
-            }
 
-            await _signInManager.RefreshSignInAsync(user);
-            
-            StatusMessage = "Your profile has been updated";
-       
-            return LocalRedirect("/Identity/Account/Manage");
-        }
-    }
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				await LoadAsync(user);
+				return Page();
+			}
+
+			var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+			if (Input.PhoneNumber != phoneNumber)
+			{
+				var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+
+				if (!setPhoneResult.Succeeded)
+				{
+					StatusMessage = "Unexpected error when trying to set phone number.";
+					return RedirectToPage();
+				}
+			}
+
+			var dbUser = _userManager.Users.FirstOrDefault(x => x.Id == user.Id && !x.IsDeleted);
+
+			if (dbUser != null)
+			{
+				dbUser.FirstName = Input.FirstName;
+				dbUser.LastName = Input.LastName;
+			}
+
+
+			if (!(await _userManager.UpdateAsync(dbUser)).Succeeded)
+			{
+
+				StatusMessage = "Unexpected error when trying to set first and last name.";
+				return RedirectToPage();
+			}
+
+
+			if (uploadPfpTask != null)
+			{
+				await uploadPfpTask;
+			}
+
+			await _signInManager.RefreshSignInAsync(user);
+
+			StatusMessage = "Your profile has been updated";
+
+			return LocalRedirect("/Identity/Account/Manage");
+		}
+	}
 }
