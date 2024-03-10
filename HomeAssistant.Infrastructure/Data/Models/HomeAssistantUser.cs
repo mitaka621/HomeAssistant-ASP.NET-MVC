0959b759f263e38f7ca84b2351a922c4382a5a13
mitaka621 <d418201@gmail.com>
2024-03-10 17:22:48 +0200

    added long and lat fileds to user

--- HomeAssistant.Infrastructure/Data/Models/HomeAssistantUser.cs
+++ HomeAssistant.Infrastructure/Data/Models/HomeAssistantUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,5 +29,13 @@ namespace HomeAssistant.Infrastructure.Data.Models
 		[Required]
         public bool IsDeleted { get; set; } = false;
 
-    }
+		[Column(TypeName = "float")]
+		[DefaultValue(42.698334)]
+        public double Latitude { get; set; }
+
+        [Column(TypeName = "float")]
+		[DefaultValue(23.319941)]
+		public double Longitude { get; set; }
+
+	}
 }
---
+++ HomeAssistant.Infrastructure/Migrations/20240310151705_User_Latitude_and_Longitude_Seeded_Sofia_defualt_location.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeAssistant.Infrastructure.Migrations
+{
+    public partial class User_Latitude_and_Longitude_Seeded_Sofia_defualt_location : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 42.698334);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 23.319941);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e2246145-9dd8-4902-ae41-68096b5ca738",
+                columns: new[] { "PasswordHash", "SecurityStamp" },
+                values: new object[] { "AQAAAAEAACcQAAAAEGaDfwnpB6Cn4C74jU2vmrPUTsFaeWq52BJ/JbErOrOitQ3zI8l1n0RNWxiMEhuwSA==", "5158ab20-d0a9-4d5a-8b0f-77221198bdd4" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e2246145-9dd8-4902-ae41-68096b5ca738",
+                columns: new[] { "PasswordHash", "SecurityStamp" },
+                values: new object[] { "AQAAAAEAACcQAAAAEDv+rDRWIbxzY+zFSMOQFad9u/nYz5Mxra/RmVRlyg0rtVVj7VCR7D80JKmE70F9xA==", "c0d7d2d2-527d-426c-ba11-cdbdf90a2eae" });
+        }
+    }
+}
