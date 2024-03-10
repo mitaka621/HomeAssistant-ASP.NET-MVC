3b0d5681d38454896d2a2de33f157e18c6d83459
mitaka621 <d418201@gmail.com>
2024-03-11 00:45:44 +0200

    svg-s added which will be displayed based on the current weather

--- HomeAssistant/wwwroot/js/weather.js
+++ HomeAssistant/wwwroot/js/weather.js
@@ -26,6 +26,36 @@ fetch('/api/Weather/Weather')
         document.querySelector(".temp").innerHTML = Math.round(json.main.temp);
         document.querySelector(".weather-icon").src = `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`
         document.querySelector(".location").innerHTML = json.name;
+        document.querySelector(".desc").innerHTML = json.weather[0].main;
+
+        let hatSvg = document.querySelector(".hat-svg");
+        let shirtSvg = document.querySelector(".shirt-svg");
+        let umbrella = document.querySelector(".umbrella-svg");
+
+        if (json.main.temp < 0) {
+            hatSvg.src = "/svg/winter-hat.svg";
+            shirtSvg.src = "/svg/heavy-jacket.svg";
+        }
+        else if (json.main.temp < 10) {
+            hatSvg.src = "/svg/winter-hat-light.svg";
+            shirtSvg.src = "/svg/jacket.svg";
+        }
+        else if (json.main.temp < 20) {
+            hatSvg.src = "/svg/winter-hat-light.svg";
+            shirtSvg.src = "/svg/Sweater.svg";
+        }
+        else if (json.main.temp < 25) {
+            hatSvg.src = "/svg/cap.svg";
+            shirtSvg.src = "/svg/shirt.svg";
+        }
+        else if (json.main.temp < 30) {
+            hatSvg.src = "~/svg/cap.svg";
+            shirtSvg.src = "/svg/t-shirt.svg";
+        }
+        else {
+            hatSvg.src = "";
+            shirtSvg.src = "/svg/tank-top.svg";
+        }
     });
     
 
