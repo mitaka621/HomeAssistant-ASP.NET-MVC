b3af930e53b39894e67e7345a6d15535543ada99
mitaka621 <d418201@gmail.com>
2024-04-04 10:31:14 +0300

    added quantity to RecipeProduct entity model

---
+++ HomeAssistant.Infrastructure/Migrations/20240403194408_add_quantity_to_RecipeProduct.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeAssistant.Infrastructure.Migrations
+{
+    public partial class add_quantity_to_RecipeProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "RecipesProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e2246145-9dd8-4902-ae41-68096b5ca738",
+                columns: new[] { "PasswordHash", "SecurityStamp" },
+                values: new object[] { "AQAAAAEAACcQAAAAEAxr5Lhwc4XQ0yRq4yHE8SfUX6H2LCgYMcHjFF0OxN0NEqnfILQymDEmmWQd8NFO9w==", "c4c1a87d-23a2-488e-831b-84b3b6af9ac2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "RecipesProducts");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "e2246145-9dd8-4902-ae41-68096b5ca738",
+                columns: new[] { "PasswordHash", "SecurityStamp" },
+                values: new object[] { "AQAAAAEAACcQAAAAELlQZCplK1vqeDGfrf10NZUqaszGoWl1RPqWLJVFiqerLc88oNp85zgKpOiNoZE5tA==", "9e644a4e-9e00-4ac1-b509-acb1a7a87aab" });
+        }
+    }
+}
