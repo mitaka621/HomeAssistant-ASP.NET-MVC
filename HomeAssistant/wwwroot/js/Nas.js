7498cf4bc7a1bd9d79dab0d957ac0e0a357ea5a1
mitaka621 <d418201@gmail.com>
2024-05-10 18:25:09 +0300

    optimized photo loading

--- HomeAssistant/wwwroot/js/Nas.js
+++ HomeAssistant/wwwroot/js/Nas.js
@@ -11,7 +11,7 @@ function LoadMoreMessages() {
             if (document.querySelector(".spinner-container")) {
                 document.querySelector(".spinner-container").remove();
             }
-          
+
             if (data.length === 0 || !document.getElementById("path").textContent) {
                 return;
             }
@@ -124,16 +124,18 @@ function handleIntersectionForPhoto(entries, observer) {
 
 let photosOnRow = [];
 function GetPhotos(skipnum = 0) {
+    window.removeEventListener('scroll', checkIfAtBottom);
+
     if (document.querySelector("button.viewer")) {
         document.querySelector("button.viewer").remove();
         document.querySelector("table").remove();
         document.querySelector("main").innerHTML += `<button class="btn btn-primary" type="button" onclick="location.reload()">Go to File Explorer</button>
     <div class="main-photo-container"></div>`;
-       
+
     }
 
     skip = skipnum;
-   
+
 
     fetch(`NAS/GetFilesJson?skip=${skip}&take=${take}&path=${document.getElementById("path").textContent}`)
         .then(r => r.json())
@@ -141,49 +143,49 @@ function GetPhotos(skipnum = 0) {
             if (document.querySelector(".spinner-container")) {
                 document.querySelector(".spinner-container").remove();
             }
-        
+
             if (data.length === 0 || !document.getElementById("path").textContent) {
                 return;
             }
             for (var i = 0; i < data.length; i++) {
-                await loadImage(data[i]);    
-               
+                await loadImage(data[i]);
+
             }
 
-            let promises = [];
 
             document.querySelectorAll("img.photo").forEach(x => {
                 x.src = "/nas/getimage?path=" + x.id;
-
-                promises.push(new Promise((resolve, reject) => {
-                    x.onload = () => {
-                        x.classList.remove("loading");
-                        resolve();
-                    }
-                }));
+                x.onload = () => {
+                    x.classList.remove("loading");
+                    resolve();
+                }
             });
 
-            await Promise.all(promises);
-
-         
-
-            document.querySelector("main").innerHTML += `<div class="spinner-container">
-				<div class="spinner-border" role="status">
-					<span class="visually-hidden">Loading...</span>
-				</div>
-			</div>`;
-
-            let observer2 = new IntersectionObserver(handleIntersectionForPhoto, options2);
-            observer2.observe(document.querySelector(".spinner-container"));
+            window.addEventListener('scroll', checkIfAtBottom);
         });
 }
 
+window.addEventListener('scroll', checkIfAtBottom);
+
+function checkIfAtBottom() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 2000) {
+        var photosToSkip = skip + take;
+        GetPhotos(photosToSkip);
+    }
+}
+
+function myFunction() {
+    console.log('You have scrolled to the bottom!');
+    window.removeEventListener('scroll', checkIfAtBottom);
+}
+
+
 let counter = 0;
 function loadImage(item) {
-    
+
 
     return new Promise((resolve, reject) => {
-       
+
         const validFormats = ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp'];
         const ext = item.path.slice(item.path.lastIndexOf('.')).toLowerCase();
 
@@ -191,7 +193,7 @@ function loadImage(item) {
             resolve();
         } else {
             const currentImg = document.createElement("img");
-            
+
             currentImg.id = item.path;
             currentImg.classList = "photo loading";
 
@@ -201,16 +203,17 @@ function loadImage(item) {
 
             currentImg.onerror = () => {
                 console.error(`Failed to load image: ${item.path}`);
-                resolve(); 
+                currentImg.src = "/svg/loading.gif";
+                resolve();
             };
 
             currentImg.src = "/svg/loading.gif";
 
             const btn = document.createElement("button");
 
-          
+
             btn.id = `b${counter++}`;
- 
+
             btn.setAttribute("onclick", "DisplayImageFull(this)");
 
             btn.classList = "btn btn-add";
@@ -220,24 +223,24 @@ function loadImage(item) {
             let height = item.height * reducePercentage;
 
             let spaceAvb = document.querySelector(".main-photo-container").getBoundingClientRect().width;
-            spaceAvb -= (photosOnRow.length - 1)*5;
+            spaceAvb -= (photosOnRow.length - 1) * 5;
             spaceAvb -= photosOnRow.reduce((total, current) => total + current.width, 0);
 
             let shouldWriteToRow = true;
 
             if (spaceAvb < 110) {
-               
+
                 photosOnRow.forEach(x => {
-                   
+
                     let newWidth = x.width + (spaceAvb / photosOnRow.length);
 
-                   
+
 
                     document.getElementById(x.obj).style.width = `${newWidth}px`;
                     document.getElementById(x.obj).querySelector("img").style.width = `${newWidth}px`;
                 })
 
-               
+
                 photosOnRow = [];
             }
             else if (spaceAvb < width) {
@@ -249,16 +252,16 @@ function loadImage(item) {
 
                 width -= (spaceToReduce / (photosOnRow.length + 1));
 
-                
+
 
                 photosOnRow.forEach(x => {
 
-                    let newWidth = x.width - (spaceToReduce / (photosOnRow.length+1));
+                    let newWidth = x.width - (spaceToReduce / (photosOnRow.length + 1));
 
                     document.getElementById(x.obj).style.width = `${newWidth}px`;
                 })
 
-                
+
                 photosOnRow = [];
             }
 
@@ -268,18 +271,18 @@ function loadImage(item) {
             btn.appendChild(currentImg);
             document.querySelector(".main-photo-container").appendChild(btn);
 
-          
+
 
             if (shouldWriteToRow) {
                 photosOnRow.push({ obj: btn.id, width: width, height: height });
             }
-           
+
         }
     });
 }
 
 function DownloadImg(path) {
-        return fetch(`/NAS/GetImage?path=${path}`);  
+    return fetch(`/NAS/GetImage?path=${path}`);
 }
 
 async function DisplayImageFull(e) {
@@ -345,13 +348,13 @@ async function DisplayImageFull(e) {
     if (prevBtn) {
         div.appendChild(prevBtn);
     }
-   
+
     div.appendChild(img);
 
     if (nextBtn) {
         div.appendChild(nextBtn);
     }
-   
+
 
     let main = document.querySelector("main");
 
@@ -364,7 +367,7 @@ function closePopUp(e) {
 }
 
 async function OpenNextPhotoFull(e) {
-    
+
     if (document.querySelector("div.popup-img")) {
 
         document.querySelector("div.popup-img>img").src = `/nas/getimage?path=${e.id}&isFull=true`;
@@ -378,7 +381,7 @@ async function OpenNextPhotoFull(e) {
                     document.querySelector(".next-btn").id = x.nextImg;
 
                     document.querySelector(".next-btn").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1c-4.2-4.5-10.1-7.1-16.3-7.1C266 128 256 138 256 150.3V208H160c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h96v57.7c0 12.3 10 22.3 22.3 22.3c6.2 0 12.1-2.6 16.3-7.1l99.9-107.1c3.5-3.8 5.5-8.7 5.5-13.8s-2-10.1-5.5-13.8L294.6 135.1z"/></svg>`;
-                                                 
+
 
                 } else {
                     document.querySelector(".next-btn").setAttribute("onclick", ``);
@@ -389,15 +392,15 @@ async function OpenNextPhotoFull(e) {
 
                 if (x.prevImg) {
 
-                        document.querySelector(".prev-btn").id = x.prevImg;
-                        document.querySelector(".prev-btn").setAttribute("onclick", `OpenNextPhotoFull(this)`);
+                    document.querySelector(".prev-btn").id = x.prevImg;
+                    document.querySelector(".prev-btn").setAttribute("onclick", `OpenNextPhotoFull(this)`);
 
-                        document.querySelector(".prev-btn").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 256a208 208 0 1 1 416 0A208 208 0 1 1 48 256zm464 0A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9c4.2 4.5 10.1 7.1 16.3 7.1c12.3 0 22.3-10 22.3-22.3V304h96c17.7 0 32-14.3 32-32V240c0-17.7-14.3-32-32-32H256V150.3c0-12.3-10-22.3-22.3-22.3c-6.2 0-12.1 2.6-16.3 7.1L117.5 242.2c-3.5 3.8-5.5 8.7-5.5 13.8s2 10.1 5.5 13.8l99.9 107.1z"/></svg>`;
+                    document.querySelector(".prev-btn").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 256a208 208 0 1 1 416 0A208 208 0 1 1 48 256zm464 0A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9c4.2 4.5 10.1 7.1 16.3 7.1c12.3 0 22.3-10 22.3-22.3V304h96c17.7 0 32-14.3 32-32V240c0-17.7-14.3-32-32-32H256V150.3c0-12.3-10-22.3-22.3-22.3c-6.2 0-12.1 2.6-16.3 7.1L117.5 242.2c-3.5 3.8-5.5 8.7-5.5 13.8s2 10.1 5.5 13.8l99.9 107.1z"/></svg>`;
 
-                    
+
                 }
                 else {
-   
+
                     document.querySelector(".prev-btn").setAttribute("onclick", ``);
 
 
@@ -449,15 +452,3 @@ function enableScroll() {
 }
 
 //
-window.addEventListener('scroll', checkIfAtBottom);
-
-function checkIfAtBottom() {
-    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight) / 4 * 3) {
-        myFunction();
-    }
-}
-
-function myFunction() {
-    console.log('You have scrolled to the bottom!');
-    window.removeEventListener('scroll', checkIfAtBottom);
-}
