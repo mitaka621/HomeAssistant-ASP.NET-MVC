2c717c0eb512b6f75a14a9174417207df58d6516
Dimitar <dimitar.georgiev@primeholding.com>
2025-05-24 20:25:02 +0300

    Port from .NET 6.0 to .NET 8.0

---
+++ HomeAssistant/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HomeAssistant.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void MigrateDatabase<TContext>(this IHost app) where TContext : DbContext
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<TContext>();
+                dbContext.Database.Migrate();
+            }
+        }
+
+    }
+}
--- UnitTests/HomeTelemetryTests.cs
+++ UnitTests/HomeTelemetryTests.cs
@@ -1,126 +1,120 @@
-﻿using HomeAssistant.Core.Services;
-using Moq.Protected;
-using Moq;
-using System.Net;
-using Microsoft.Extensions.Configuration;
+﻿using System.Net;
 using HomeAssistant.Core.Contracts;
-using Microsoft.Extensions.Logging;
+using HomeAssistant.Core.Services;
 using HomeAssistant.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Protected;
 
 namespace UnitTests
 {
-	[TestFixture]
-	public class HomeTelemetryTests
-	{
-		private HomeAssistantDbContext _dbContext;
-		private IHomeTelemetryService _homeTelemetryService;
-		private Mock<HttpMessageHandler> _mockHttpMessageHandler;
-		private HttpClient _httpClient;
-		private Mock<IConfiguration> _mockConfiguration;
-		private Mock<ILogger<IHomeTelemetryService>> _logger;
-		private INotificationService _notificationService;
-		private Mock<ILogger<INotificationService>> _mockLogger;
+    [TestFixture]
+    public class HomeTelemetryTests
+    {
+        private HomeAssistantDbContext _dbContext;
+        private IHomeTelemetryService _homeTelemetryService;
+        private Mock<HttpMessageHandler> _mockHttpMessageHandler;
+        private HttpClient _httpClient;
+        private Mock<IConfiguration> _mockConfiguration;
+        private Mock<ILogger<IHomeTelemetryService>> _logger;
+        private INotificationService _notificationService;
+        private Mock<ILogger<INotificationService>> _mockLogger;
 
-		[SetUp]
-		public void Setup()
-		{
-			var options = new DbContextOptionsBuilder<HomeAssistantDbContext>()
-				.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-				.Options;
-			_dbContext = new HomeAssistantDbContext(options);
+        [SetUp]
+        public void Setup()
+        {
+            _mockConfiguration = new Mock<IConfiguration>();
+            _mockLogger = new Mock<ILogger<INotificationService>>();
 
-			_mockHttpMessageHandler = new Mock<HttpMessageHandler>();
-			_mockHttpMessageHandler
-				.Protected()
-				.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-				.ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK });
+            var options = new DbContextOptionsBuilder<HomeAssistantDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _dbContext = new HomeAssistantDbContext(options);
 
+            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            _mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.OK });
 
-			_httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+            _httpClient = new HttpClient(_mockHttpMessageHandler.Object);
 
-			_logger=new Mock<ILogger<IHomeTelemetryService>>();
+            _logger = new Mock<ILogger<IHomeTelemetryService>>();
 
-			_notificationService =new NotificationService(_dbContext, new Mock<IUserService>().Object, new Mock<IimageService>().Object, _mockConfiguration.Object, _mockLogger.Object);
+            _notificationService = new NotificationService(_dbContext, new Mock<IUserService>().Object, new Mock<IimageService>().Object, _mockConfiguration.Object, _mockLogger.Object);
 
-			
+            _mockConfiguration = new Mock<IConfiguration>();
 
-			_mockConfiguration = new Mock<IConfiguration>();
+            _mockConfiguration.Setup(x => x.GetSection("HomeTelemetryServerIp").Value).Returns("apikey");
 
-			_mockConfiguration.Setup(x => x.GetSection("HomeTelemetryServerIp").Value).Returns("apikey");
+            _homeTelemetryService = new HomeTelemetryService(
+                _mockConfiguration.Object,
+                _httpClient,
+                _dbContext,
+                _logger.Object,
+                _notificationService);
+        }
 
-			_homeTelemetryService = new HomeTelemetryService(
-				_mockConfiguration.Object,
-				_httpClient,
-				_dbContext,
-				_logger.Object,
-				_notificationService);
-		}
+        [TearDown]
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+            _httpClient.Dispose();
+        }
 
-		[Test]
-		public async Task GetData_Returns_HomeTelemetry_Gathered_Data()
-		{
-			string mockData = "mocked location data";
+        [Test]
+        public async Task GetData_Returns_HomeTelemetry_Gathered_Data()
+        {
+            string mockData = "mocked location data";
 
-			_mockHttpMessageHandler
-				.Protected()
-				.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-				.ReturnsAsync(new HttpResponseMessage
-				{
-					StatusCode = HttpStatusCode.OK,
-					Content = new StringContent(mockData)
-				});
+            _mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(mockData)
+                });
 
-			string result = await _homeTelemetryService.GetLiveData();
+            string result = await _homeTelemetryService.GetLiveData();
 
-			Assert.AreEqual(mockData, result);
-		}
+            Assert.That(result, Is.EqualTo(mockData));
+        }
 
-		[Test]
-		public async Task SaveData_SavesDataToDatabase()
-		{
-			var testData = "{\"CPM\": 100, \"Temperature\": 25, \"Humidity\": 50, \"Radiation\": 2}";
+        [Test]
+        public async Task SaveData_SavesDataToDatabase()
+        {
+            var testData = "{\"CPM\": 100, \"Temperature\": 25, \"Humidity\": 50, \"Radiation\": 2}";
 
-			  
-			await _homeTelemetryService.SaveData(testData);
+            await _homeTelemetryService.SaveData(testData);
 
-			 
-			var savedData = await _dbContext.homeTelemetries.FirstOrDefaultAsync();
-			Assert.IsNotNull(savedData);
+            var savedData = await _dbContext.homeTelemetries.FirstOrDefaultAsync();
+            Assert.That(savedData, Is.Not.Null);
+        }
 
-		}
+        [Test]
+        public async Task CreateNotificationIfDataIsAbnormal_DoesntSendsNotification()
+        {
+            var testData = "{\"CPM\": 100, \"Temperature\": 25, \"Humidity\": 50, \"Radiation\": 0.5}";
 
-		[Test]
-		public async Task CreateNotificationIfDataIsAbnormal_DoesntSendsNotification()
-		{
-			  
-		var testData = "{\"CPM\": 100, \"Temperature\": 25, \"Humidity\": 50, \"Radiation\": 0.5}";
+            var result = await _homeTelemetryService.CreateNotificationIfDataIsAbnormal(testData);
 
-			  
-			var result = await _homeTelemetryService.CreateNotificationIfDataIsAbnormal(testData);
+            Assert.That(await _dbContext.Notifications.CountAsync(), Is.EqualTo(0));
+            Assert.That(result, Is.EqualTo(-1));
+        }
 
-			 
-			Assert.AreEqual(0, await _dbContext.Notifications.CountAsync());
-			Assert.AreEqual(-1, result);
+        [Test]
+        public async Task CreateNotificationIfDataIsAbnormal_SendsNotification()
+        {
+            var testData = "{\"CPM\": 100, \"Temperature\": 25, \"Humidity\": 50, \"Radiation\": 100}";
 
-		}
+            var result = await _homeTelemetryService.CreateNotificationIfDataIsAbnormal(testData);
 
-		[Test]
-		public async Task CreateNotificationIfDataIsAbnormal_SendsNotification()
-		{
-
-			var testData = "{\"CPM\": 100, \"Temperature\": 25, \"Humidity\": 50, \"Radiation\": 100}";
-
-			  
-			var result = await _homeTelemetryService.CreateNotificationIfDataIsAbnormal(testData);
-
-			Assert.AreNotEqual(-1, result);
-			Assert.AreEqual(1, await _dbContext.Notifications.CountAsync());
-
-			Assert
-				.AreEqual("Abnormal radiation levels detected", (await _dbContext.Notifications.FirstAsync(x => x.Id == result)).Title);
-		}
-
-
-	}
+            Assert.That(result, Is.Not.EqualTo(-1));
+            Assert.That(await _dbContext.Notifications.CountAsync(), Is.EqualTo(1));
+            Assert.That((await _dbContext.Notifications.FirstAsync(x => x.Id == result)).Title, Is.EqualTo("Abnormal radiation levels detected"));
+        }
+    }
 }
